94a098932a2b8440a332acabebcd5fac75723bfb
Tom Mortimer-Jones <tom@morty.co.uk>
2017-03-27 12:38:41 +0100

    Pull lat/lon from data file

--- config.rb
+++ config.rb
@@ -15,8 +15,11 @@ page '/*.txt', layout: false
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
-data.places.locations.each do |name|
-      proxy "/#{name}.html", "/index.html", :locals => { :location_name => name }
+data.places.locations.each do |pack|
+  name = pack[0]
+  lat = pack[1]
+  lon = pack[2]
+  proxy "/#{name}.html", "/index.html", :locals => { :location_name => name, :lat => lat, :lon => lon }
 end
 
 # General configuration
